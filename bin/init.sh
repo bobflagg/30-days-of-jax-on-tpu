7b0fbefc44e800eb3512aef35f12d869966929d5
Bob Flagg <calcworks@gmail.com>
2022-06-10 23:03:21 +0000

    Added some setup utilities.

---
+++ bin/init.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh -O ~/anaconda.sh
+bash ~/anaconda.sh -b -p $HOME/anaconda
+
+export PATH=$HOME/anaconda/bin:$PATH
+conda init
+
+cd $HOME
+source .bashrc
+
+pip install "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+cd
+git clone https://github.com/google/flax.git
+pip install --user -e flax
+
+
+
---
+++ bin/start-jupyter.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+jupyter notebook --generate-config
+cat ./jupyter-config.txt >> /home/rflagg/.jupyter/jupyter_notebook_config.py
+jupyter-notebook --no-browser --ip=0.0.0.0 --port=8080
+
