158bad0ddee1d8c4d3bfc43e679d45300af117a4
Bob Flagg <calcworks@gmail.com>
2022-06-13 15:38:13 -0400

    Added setting python path.

--- bin/start-jupyter.sh
+++ bin/start-jupyter.sh
@@ -3,5 +3,6 @@
 jupyter notebook --generate-config
 cat ./jupyter-config.txt >> /home/rflagg/.jupyter/jupyter_notebook_config.py
 cd
+export PYTHONPATH=~/30-days-of-jax-on-tpu/python
 jupyter-notebook --no-browser --ip=0.0.0.0 --port=8080
 
